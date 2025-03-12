1b98c29f315185dc868631900a3de78c0df94c01
minyeong-lee <lmy1366@gmail.com>
2025-03-12 18:01:39 +0900

    테스트 코드 구현

--- src/test/java/com/missions/a_str_length/ApplicationTests.java
+++ src/test/java/com/missions/a_str_length/ApplicationTests.java
@@ -1,6 +1,16 @@
 package com.missions.a_str_length;
 
-public class ApplicationTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class ApplicationTests {
+    @Test
+    void length() {
+        String result = Application.countLength("Java is Fun");
+
+        // 출력 기대값
+        // "단어1: 4, 단어2: 2, 단어3: 3, 총 길이: 9"
+        Assertions.assertEquals("단어1: 4, 단어2: 2, 단어3: 3, 총 길이: 9", result);
+    }
 
 }
